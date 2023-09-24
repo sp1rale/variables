fdda449361ce019608a41468d6e480e0a7462211
sp1rale <dimayaremchyk24@gmail.com>
2023-09-24 17:33:21 +0300

    ex 2 task was done

--- main.py
+++ main.py
@@ -1,28 +1,8 @@
-month_number = int(input("Введіть номер місяця (1-12): "))
+number = float(input("Введіть число: "))
 
-if month_number == 1:
-    print("січень")
-elif month_number == 2:
-    print("лютий")
-elif month_number == 3:
-    print("березень")
-elif month_number == 4:
-    print("квітень")
-elif month_number == 5:
-    print("травень")
-elif month_number == 6:
-    print("червень")
-elif month_number == 7:
-    print("липень")
-elif month_number == 8:
-    print("серпень")
-elif month_number == 9:
-    print("вересень")
-elif month_number == 10:
-    print("жовтень")
-elif month_number == 11:
-    print("листопад")
-elif month_number == 12:
-    print("грудень")
+if number > 0:
+    print("Number is positive")
+elif number < 0:
+    print("Number is negative")
 else:
-    print("Некоректний номер")
+    print("Number is equal to zero")
