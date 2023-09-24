23c2e8a7f40321844857512a4c4eb3776f1bcb3c
sp1rale <dimayaremchyk24@gmail.com>
2023-09-24 17:29:25 +0300

    ex 1 task was done

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
-try:
-    print()
-except Exception as e:
-    print(e)
+month_number = int(input("Введіть номер місяця (1-12): "))
+
+if month_number == 1:
+    print("січень")
+elif month_number == 2:
+    print("лютий")
+elif month_number == 3:
+    print("березень")
+elif month_number == 4:
+    print("квітень")
+elif month_number == 5:
+    print("травень")
+elif month_number == 6:
+    print("червень")
+elif month_number == 7:
+    print("липень")
+elif month_number == 8:
+    print("серпень")
+elif month_number == 9:
+    print("вересень")
+elif month_number == 10:
+    print("жовтень")
+elif month_number == 11:
+    print("листопад")
+elif month_number == 12:
+    print("грудень")
+else:
+    print("Некоректний номер")
