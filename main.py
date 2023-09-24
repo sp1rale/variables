43b9e29ecb41c33d9d635d802554cde75684ca69
sp1rale <dimayaremchyk24@gmail.com>
2023-09-24 18:00:00 +0300

    ex 3 task was done

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-total_seconds = int(input("Введіть час у секундах: "))
+price_per_console = float(input("Введіть вартість однієї ігрової приставки: "))
+quantity = int(input("Введіть кількість ігрових приставок: "))
+discount_percentage = float(input("Введіть відсоток знижки (0-100): "))
 
-hours = total_seconds // 3600
-minutes = (total_seconds % 3600) // 60
-seconds = total_seconds % 60
-
-print(f"До опівночі залишилося {hours} годин, {minutes} хвилин і {seconds} секунд.")
+choice = input("Оберіть операцію (замовлення або вартість однієї приставки зі знижкою")
+if choice == "замовлення":
+    total_price = price_per_console * quantity * (1 - discount_percentage / 100)
+    print(f"Загальна сума замовлення: {total_price:.2f} грн")
+elif choice == "вартість однієї приставки зі знижкою":
+    discounted_price = price_per_console * (1 - discount_percentage / 100)
+    print(f"Вартість однієї приставки зі знижкою: {discounted_price:.2f} грн")
+else:
+    print("Ви ввели некоректний вибір операції. Оберіть 'замовлення' або 'вартість однієї приставки зі знижкою'.")
