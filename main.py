f7c811b0234fac6db67dcec030954e29e94d5d92
sp1rale <dimayaremchyk24@gmail.com>
2023-09-24 17:43:28 +0300

    ex 1 task was done

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-number1 = float(input("Введіть перше число: "))
+total_seconds = int(input("Введіть час у секундах: "))
 
-number2 = float(input("Введіть друге число: "))
+hours = total_seconds // 3600
+minutes = (total_seconds % 3600) // 60
+seconds = total_seconds % 60
 
-if number1 == number2:
-    print("Числа рівні.")
-else:
-
-    if number1 < number2:
-        print("Числа у порядку зростання:", number1, number2)
-    else:
-        print("Числа у порядку зростання:", number2, number1)
+print(f"До опівночі залишилося {hours} годин, {minutes} хвилин і {seconds} секунд.")
