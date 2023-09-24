8cd10544e36ac70550dbb3b3dfd05c343a9a9c22
sp1rale <dimayaremchyk24@gmail.com>
2023-09-24 17:54:49 +0300

    ex 2 task was done

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-try:
-    print()
-except Exception as e:
-    print(e)
+import math
+
+diameter = float(input("Введіть діаметр кола: "))
+
+choice = input("Оберіть операцію (площа або периметр): ")
+
+radius = diameter / 2
+
+if choice == "площа":
+    area = math.pi * radius ** 2
+    print(f"Площа кола з діаметром {diameter} дорівнює {area:.2f} квадратних одиниць.")
+elif choice == "периметр":
+    perimeter = 2 * math.pi * radius
+    print(f"Периметр кола з діаметром {diameter} дорівнює {perimeter:.2f} одиниць довжини.")
+else:
+    print("Ви ввели некоректний вибір операції. Оберіть 'площа' або 'периметр'.")
